c24c3438d488275ecb4e87accc841a29ee95bd0a
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-07-22 20:47:00 +0800

    feat: create and use fisher yates algoirithm for card sorting

---
+++ src/utilities/shuffle.js
@@ -0,0 +1,21 @@
+
+
+const shuffle = () => {
+
+  const images = [
+    {image: ('/images/Cards/1.jpg')},
+    {image: '/images/Cards/2.jpg'},
+    {image: '/images/Cards/3.jpg'},
+    {image: '/images/Cards/4.jpg'},
+    {image: '/images/Cards/5.jpg'},
+    {image: '/images/Cards/6.jpg'},
+    {image: '/images/Cards/7.jpg'},
+    {image: '/images/Cards/8.jpg'},
+  ]
+
+  return [...images, ...images]
+  .sort(() => Math.random() - 0.5)
+  .map((card, index) => ({...card, id: `card-${index}`}))
+}
+
+export default shuffle
